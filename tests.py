5a74830434eb5819bf20a5fe7165523070211913
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-08-16 21:14:16 +0100

    Test the numpy euler constant

--- tests.py
+++ tests.py
@@ -1,6 +1,10 @@
 import numpy as np
 
+# TODO: TEST 1
 k = 3
 distances = [10, 4, 9, 2, 5, 6, 7, 8, 3, 1]
 indices = np.argsort(distances)[:k]  # sort and return the index from the original array
 print(indices)
+
+# TODO: TEST 2
+print(np.e)
