493164f8b0f70a5e302607092f44714d89aa6ce6
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-08-17 16:49:36 +0100

    Test the numpy bincount method

--- tests.py
+++ tests.py
@@ -2,9 +2,14 @@ import numpy as np
 
 # TODO: TEST 1
 k = 3
-distances = [10, 4, 9, 2, 5, 6, 7, 8, 3, 1]
-indices = np.argsort(distances)[:k]  # sort and return the index from the original array
+dist = [10, 4, 9, 2, 5, 6, 7, 8, 3, 1]
+indices = np.argsort(dist)[:k]  # sort and return the indices from the original array
 print(indices)
 
 # TODO: TEST 2
-print(np.e)
+print(np.e)  # euler constant
+
+# TODO: TEST 3
+y = np.array([4, 5, 6, 5, 4])
+hist = np.bincount(y)  # how many times the index occured as an item in the list
+print(hist)
